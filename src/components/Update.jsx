3f606110414582b51b085f146cf02d1587449c20
iamtonmoy0 <tonmoy2code@gmail.com>
2023-06-02 13:18:38 -0400

    feat: data update to db added

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Update = () => {
 	const user = useLoaderData()
@@ -8,15 +10,33 @@ const handleUpdate=e=>{
 	const name=form.name.value;
 	const email=form.email.value;
 	console.log(name,email)
+	const updatedUser={name,email}
+	fetch(`http://localhost:5000/update/${user._id}`,{
+		method:'PUT',
+		headers:{
+			'content-type':'application/json'
+		},
+		body:JSON.stringify(updatedUser)
+	})
+	.then(res=>res.json)
+	.then(data=>{
+		console.log(data)
+		if(data.modifiedCount>0){
+			toast.success('data updated')
+		}
+	
+	
+	})
 }
 
 	return (
 		<div>
+			<ToastContainer/>
 			
 			<form onSubmit={handleUpdate}>
 				Current name : {user.name}
 			<input name="name" type="text" placeholder="Enter name" />
-			Current email : {user.name}
+			Current email : {user.email}
 			<input type="email" name="email" placeholder="enter email" />
 			<button type="submit">Update</button>
 
